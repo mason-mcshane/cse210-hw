2e677c1dd7d82190d7267bebd2815d8c6d75a790
mason-mcshane <mcs20002@byui.edu>
2024-05-04 15:13:45 -0600

    Completed the weekly assignment.

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -3,9 +3,24 @@ public class Entry
     public string _date;
     public string _promptText;
     public string _entryText;
+    public Prompts _promptGeneator = new Prompts();
 
     public void Display()
     {
-
+        Console.WriteLine($"Date: {_date} - Prompt: {_promptText}");
+        Console.WriteLine(_entryText);
+    }
+    public Entry() 
+    {
+        this._date = DateTime.Now.ToString("MM-dd-yyyy");
+        _promptText = _promptGeneator.GenerateRandomPrompt();
+        Console.WriteLine(_promptText);
+        _entryText = Console.ReadLine();
+    }
+    public Entry(string date, string prompt, string entry)
+    {
+        this._date = date;
+        _promptText = prompt;
+        _entryText = entry;
     }
 }   
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,13 +1,49 @@
 using System;
+using System.Net.Mail;
 
 class Program
 {
     static void Main(string[] args)
     {
         Journal theJournal = new Journal();
-        Entry anEntry = new Entry();
-        anEntry.Display();
 
-        Console.WriteLine("Hello Develop02 World!");
+        bool isDone = false;
+
+        while (!isDone)
+        {
+
+            Console.WriteLine("Please Select from the list of choices:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display your entry");
+            Console.WriteLine("3. Save your entry");
+            Console.WriteLine("4. Load entries");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("What would you like to do?");
+            
+            string Userinput = Console.ReadLine();
+
+            switch(Userinput)
+            {
+                case "1":
+                    Entry entry = new Entry();
+                    theJournal.AddEntry(entry);
+                    break;
+                case "2":
+                    theJournal.DisplayAll();
+                    break;
+                case "3":
+                    theJournal.SaveToFile();
+                    break;
+                case "4":
+                    theJournal.LoadFromFile();
+                    break;
+                case "5":
+                    isDone = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice, please try again.");
+                    break;
+            }    
+        }
     }
 }
