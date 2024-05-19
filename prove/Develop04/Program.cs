92a549be480b613a2e1e0864a13e5a595fcd86ec
mason-mcshane <mcs20002@byui.edu>
2024-05-19 00:20:45 -0600

    other

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,5 +1,6 @@
 using System;
-
+// Gave the program a cleaner organization by adding in a Landing page that essentially
+// acts as the new program.cs file for the assignment.
 class Program
 {
     static void Main(string[] args)
