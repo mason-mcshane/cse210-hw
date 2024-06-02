0a21b6f78ed7201c7f9de628e072bc3c2e824ad8
mason-mcshane <mcs20002@byui.edu>
2024-06-01 23:09:27 -0600

    polishing

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -7,31 +7,30 @@ class Program
     {
         List<Video> videos = new List<Video>();
 
-        Video v1 = new Video ("Human Anatomy", "SNC Ikedi", 300);
-        v1._comments.Add(new Comment("", "Great video"));
-        v1._comments.Add(new Comment("", "Very informative"));
-        v1._comments.Add(new Comment("", "Could be more detailed"));
-        v1._comments.Add(new Comment("","Thank you for this video"));
+        Video v1 = new Video ("Destiny 2 Raid Guide Video #1", "Ben Robinson", 300);
+        v1._comments.Add(new Comment("Anne", "Very informative!"));
+        v1._comments.Add(new Comment("Billy", "Great Job!"));
+        v1._comments.Add(new Comment("Clark", "Thank you for your hard work!"));
+        v1._comments.Add(new Comment("Dale","Solid animation!"));
 
 
-        Video v2 = new Video ("Human Physiology", "JC Okeke", 200);
-        v2._comments.Add(new Comment("", "Interesting video"));
-        v2._comments.Add(new Comment("", "Good explanation"));
-        v2._comments.Add(new Comment("", "Clear and concise"));
-        v2._comments.Add(new Comment("", "Well done"));
-        v2._comments.Add(new Comment("", "Clear and concise"));
-        v2._comments.Add(new Comment("", "Well done"));
+        Video v2 = new Video ("Destiny 2 Raid Guide Video #2", "Philip Tomlin", 200);
+        v2._comments.Add(new Comment("Edward", "Very visual!"));
+        v2._comments.Add(new Comment("Fred", "I learned a lot from this!"));
+        v2._comments.Add(new Comment("George", "1st comment!"));
+        v2._comments.Add(new Comment("Hale", "Can you try explaining this concept next?"));
 
-        Video v3 = new Video ("Intro to Human biology", "JC Okeke", 500);
-        v3._comments.Add(new Comment("", "Interesting video"));
-        v3._comments.Add(new Comment("", "Good Animation"));
-        v3._comments.Add(new Comment("", "very clear and concise"));
-        v3._comments.Add(new Comment("", "Well taught"));
+        Video v3 = new Video ("Destiny 2 Raid Guide Video #3", "Dang Long", 500);
+        v3._comments.Add(new Comment("Issac", "I like this activity!"));
+        v3._comments.Add(new Comment("Julian", "Had so much fun with my friends!"));
+        v3._comments.Add(new Comment("Kearon", "10/10 Video!"));
+        v3._comments.Add(new Comment("Lemuel", "Hasn't this idea been done before?"));
 
-        Video v4 = new Video ("Human Cell and organs", "JC Okeke", 100);
-        v4._comments.Add(new Comment("", "Facinating video"));
-        v4._comments.Add(new Comment("", "Good explanation"));
-        v4._comments.Add(new Comment("", "Clear and concise"));
+        Video v4 = new Video ("Destiny 2 Raid Guide Video #4", "Paul Berkey", 100);
+        v4._comments.Add(new Comment("Michael", "How long did it take you to make this?"));
+        v4._comments.Add(new Comment("Nathan", "Which class do you like best?"));
+        v4._comments.Add(new Comment("Oliver", "How long did it take you to beat this activity?"));
+        v4._comments.Add(new Comment("Phil", "Can you get a team to help me out sometime?"));
        
         videos.Add(v1);
         videos.Add(v2);
